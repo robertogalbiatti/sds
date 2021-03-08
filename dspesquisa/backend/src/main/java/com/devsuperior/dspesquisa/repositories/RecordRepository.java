2051167e775e7d6ae50a8413341651e07c3b0202
Roberto <roberto.galbiatti@gmail.com>
2021-03-08 16:57:09 -0300

    Adding some changes

--- dspesquisa/backend/src/main/java/com/devsuperior/dspesquisa/repositories/RecordRepository.java
+++ dspesquisa/backend/src/main/java/com/devsuperior/dspesquisa/repositories/RecordRepository.java
@@ -1,10 +1,18 @@
 package com.devsuperior.dspesquisa.repositories;
 
+import java.time.Instant;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.devsuperior.dspesquisa.entities.Record;
 @Repository
 public interface RecordRepository extends JpaRepository<Record, Long>{
+
+	@Query("SELECT obj FROM Record obj") //esse é o padrão para utilizar esse Query
+	Page<Record> findByMoments(Instant min, Instant max, PageRequest pageRequest);
 
 }
