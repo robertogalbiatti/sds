23c5a9163789ee3245ebf839044277edec6745a8
Betão <robertogalbiatti@gmail.com>
2021-03-21 17:16:25 -0300

    Finishing POST

--- dspesquisa/backend/src/main/java/com/devsuperior/dspesquisa/repositories/RecordRepository.java
+++ dspesquisa/backend/src/main/java/com/devsuperior/dspesquisa/repositories/RecordRepository.java
@@ -3,7 +3,7 @@ package com.devsuperior.dspesquisa.repositories;
 import java.time.Instant;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -12,7 +12,9 @@ import com.devsuperior.dspesquisa.entities.Record;
 @Repository
 public interface RecordRepository extends JpaRepository<Record, Long>{
 
-	@Query("SELECT obj FROM Record obj") //esse é o padrão para utilizar esse Query
-	Page<Record> findByMoments(Instant min, Instant max, PageRequest pageRequest);
+	@Query("SELECT obj FROM Record obj WHERE "//esse é o padrão para utilizar esse Query
+			+ "(coalesce(:min, null) IS NULL or obj.moment >= :min) AND " //garantindo que se for null o mínimo, é verdadeiro o primeiro (), assim como na linha de baixo.
+			+ "(coalesce(:max, null) IS NULL or obj.moment <= :max)") //coalesce é uma peculiaridade do PostreSQL
+	Page<Record> findByMoments(Instant min, Instant max, Pageable pageable);
 
 }
